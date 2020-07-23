cb90e796cf1d27b784e6a3544364fe99a9533923
yameholo <ame.aroid.xz5@gmail.com>
2020-07-23 22:03:55 +0900

    Add とりあえずの構成

---
+++ app/Services/FakerService.py
@@ -0,0 +1,42 @@
+from faker import Faker
+from faker.config import AVAILABLE_LOCALES
+from Services.FakerError import FakerError
+DEFAULT_LANG = 'ja_JP'
+
+
+class FakerService:
+    LANG = 'lang'
+    SEED = 'seed'
+
+    def __init__(self, lang=DEFAULT_LANG, seed=None):
+        self.__error = None
+        self.validate(lang=lang, seed=seed)
+
+        if (self.error is None):
+            self.faker = Faker(lang)
+            self.faker.seed_instance(seed)
+
+    def execute(self):
+        raise NotImplementedError
+
+    def validate(self, **kwargs):
+        for key, item in kwargs.items():
+            if (key == self.LANG):
+                self.__validate_lang(item)
+            elif (key == self.SEED):
+                self.__validation_seed(item)
+
+    def __validate_lang(self, value):
+        if (value not in AVAILABLE_LOCALES):
+            self.__set_error(FakerError.NOT_SUPPORTED_LANG)
+
+    def __validation_seed(self, value):
+        if (not isinstance(value, int)):
+            self.__set_error(FakerError.NOT_INTEGER_TYPE)
+
+    def __set_error(self, err):
+        self.__error = err
+
+    @property
+    def error(self):
+        return self.__error
--- app/index.py
+++ app/index.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from faker import Faker
+from faker.config import AVAILABLE_LOCALES
+from Services.FakerService import FakerService
+from Services.FakerError import FakerError
 import MeCab
 
 app = Flask(__name__)
 mecab = MeCab.Tagger()
+LANG = 'ja_JP'
 
 
 @app.route('/')
 def hello():
     name = "こんにちは世界。"
     return mecab.parse(name)
+
+
+@app.route('/name', methods=['GET'])
+def name():
+    lang = request.args.get('lang', LANG)
+    seed = request.args.get('seed', None)
+
+    service = FakerService(lang, seed)
+    if (service.error in (FakerError.NOT_SUPPORTED_LANG, FakerError.NOT_INTEGER_TYPE)):
+        return {'message': 'error'}
+
+    return service.faker.name()
 
 
 if __name__ == "__main__":
